39e072abe8916a65ea4dd5b9b385cfd7da288967
WKKKKKKKKKKKKKK <weikang.kong@kaust.edu.sa>
2026-01-28 17:56:08 +0300

    Update assignment 1 code

--- assignment_1.py
+++ assignment_1.py
@@ -1,14 +1,21 @@
+### Assignment 1: Visualizing SRTM Elevation Data
+## import necessary libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import pdb
 import xarray as xr
 
-dset=xr.open_dataset('N21E039.SRTMGL1_NC.nc')
 
-DEM = np.array(dset.variables["SRTMGL1_DEM"])
+dset = xr.open_dataset('N21E039.SRTMGL1_NC.nc')           # load the SRTM data
+#pdb.set_trace()                                          # add breakpoint here if needed
 
-plt.imshow(DEM)
+DEM = np.array(dset.variables['SRTMGL1_DEM'])             # extract the elevation data as a numpy array
+dset.close()                                              # close the dataset
+#pdb.set_trace()                                          # add breakpoint here if needed
+
+plt.imshow(DEM)                                           # visualize the DEM using imshow
 cbar = plt.colorbar()
 cbar.set_label('Elevation (m asl)')
+plt.savefig('assignment_1.png', dpi=300)
 plt.show()
